c9fb7b08e75843cb0c906f9f71e5e22cfe64f193
Amaliamelo <amalia.melo@aluno.ifsp.edu.br>
2020-08-26 18:47:02 -0300

    20200826

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
     echo'<h3>Pagina de links de atividades da <b>AMALIA MELO</b></h3';
 
     echo'<p><ul>
-        <li><a href="20200812/index.php">Editor de Texto</li><hr />
+        <li><a href="20200812/index.php">Editor de Texto - JEQUERY</li><hr />
         <li><a href="20200819/index.php">Engenharia Reversa</a></li><hr />
-        <li><a href="20200825/index.php">Engenharia Reversa - AJAX_JEQUERY</a></li>
+        <li><a href="20200825/index.php">Engenharia Reversa - AJAX_JEQUERY</a></li><hr />
+        <li><a href="20200826/index.php">Editor de Texto - AJAX_JEQUERY</a></li>
     </ul></p>';
 ?>
